295cd2686a7f23731e8fc45e7bb12a47b879c50c
Moughit Moutaouakkil <57865083+MouGhiTG@users.noreply.github.com>
2021-12-19 16:45:05 +0100

    Create card.js

---
+++ script/card.js
@@ -0,0 +1,13 @@
+
+	VanillaTilt.init(document.querySelectorAll(".card"), {
+		max: 25,
+		speed: 400
+	});
+	VanillaTilt.init(document.querySelectorAll(".tools-container"), {
+		max: 15,
+		speed: 400
+	});
+	VanillaTilt.init(document.querySelectorAll(".clock"), {
+		max: 25,
+		speed: 400
+	});
